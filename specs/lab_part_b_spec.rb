08b11fa2b55ea9be8298e4253f029e9e5dc0a3f4
Gary Clark <gclark04432@live.co.uk>
2019-11-25 14:44:07 +0000

    part b starts

---
+++ specs/lab_part_b_spec.rb
@@ -0,0 +1,8 @@
+require ('minitest/autorun')
+require ('minitest/reporters')
+require_relative ('../lab_part_b')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class  < Minitest::Test
+
+end
