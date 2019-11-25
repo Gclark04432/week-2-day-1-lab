f0d92b488e4b1dd5e835ad1f9e01c0439bf63305
Gary Clark <gclark04432@live.co.uk>
2019-11-25 14:29:04 +0000

    completed_second_test git push -u origin master git push

--- specs/lab_part_a_spec.rb
+++ specs/lab_part_a_spec.rb
@@ -5,11 +5,20 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class TestStudent < Minitest::Test
 
-  def test_can_get_student_name
+  def test_can_get_student_details
     student = Student.new("Silvia", "E36")
     assert_equal("Silvia", student.name())
+    assert_equal("E36", student.cohort())
   end
 
+  def test_can_change_student_details
+    student = Student.new("Silvia", "E36")
+    student.name = "Gary"
+    student.cohort = "E35"
+
+    assert_equal("E35", student.cohort())
+    assert_equal("Gary", student.name())
+  end
 
 
 end
