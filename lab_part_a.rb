9378e64934f206ded6e7dee345526c3862d337c2
Gary Clark <gclark04432@live.co.uk>
2019-11-25 14:40:45 +0000

    completed-part-a

--- lab_part_a.rb
+++ lab_part_a.rb
@@ -1,11 +1,19 @@
 class Student
 
-attr_accessor :name, :cohort
+attr_accessor :name, :cohort, :language
 
-def initialize(name, cohort)
+def initialize(name, cohort, language)
   @name = name
   @cohort = cohort
+  @language = language
 end
 
+def talk()
+  return "I can talk!"
+end
+
+def students_favourite_language()
+  return "I Love #{@language}"
+end
 
 end
--- specs/lab_part_a_spec.rb
+++ specs/lab_part_a_spec.rb
@@ -6,13 +6,13 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 class TestStudent < Minitest::Test
 
   def test_can_get_student_details
-    student = Student.new("Silvia", "E36")
+    student = Student.new("Silvia", "E36", "")
     assert_equal("Silvia", student.name())
     assert_equal("E36", student.cohort())
   end
 
   def test_can_change_student_details
-    student = Student.new("Silvia", "E36")
+    student = Student.new("Silvia", "E36", "")
     student.name = "Gary"
     student.cohort = "E35"
 
@@ -20,5 +20,14 @@ class TestStudent < Minitest::Test
     assert_equal("Gary", student.name())
   end
 
+  def test_student_can_talk
+    student = Student.new("Silvia", "E36", "")
+    assert_equal("I can talk!", student.talk())
+  end
+
+  def test_student_programming_language
+    student = Student.new("Silvia", "E36", "Ruby")
+    assert_equal("I Love Ruby", student.students_favourite_language())
+  end
 
 end
